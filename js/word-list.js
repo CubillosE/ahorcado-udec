2b387334fc5230213456bc26e20a7586a6aad19c
eerasmocubillos <eerasmocubillos@ucundinamarca.edu.co>
2025-05-23 09:38:56 -0500

    Palabras añadidas

--- js/word-list.js
+++ js/word-list.js
@@ -40,6 +40,46 @@ const palabras = [
         word: "clap",
         hint: "Golpear las palmas de las manos para animar."
     },
+    {
+        word: "extension",
+        hint: "Cuando los bases levantan al flyer con los brazos completamente extendidos."
+    },
+    {
+        word: "cradle",
+        hint: "Manera de atrapar al flyer después de un salto o lanzamiento."
+    },
+    {
+        word: "stunt",
+        hint: "Una figura o acrobacia realizada en grupo."
+    },
+    {
+        word: "fly",
+        hint: "El acto de ser levantado en el aire durante un stunt."
+    },
+    {
+        word: "kickline",
+        hint: "Formación en línea donde todas las cheerleaders hacen patadas sincronizadas."
+    },
+    {
+        word: "jump",
+        hint: "Salto realizado por las cheerleaders en una rutina."
+    },
+    {
+        word: "cheerleader",
+        hint: "Persona que anima y motiva a un equipo con cánticos y acrobacias."
+    },
+    {
+        word: "tumble",
+        hint: "Serie de movimientos gimnásticos como volteretas y saltos."
+    },
+    {
+        word: "spot",
+        hint: "Ubicación donde los cheerleaders se posicionan para realizar una acrobacia o salto."
+    },
+    {
+        word: "cheer",
+        hint: "Cántico o grito de apoyo para el equipo."
+    },
 ];
 
 export default palabras;
